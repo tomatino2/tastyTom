81e97e418b49038ac80849d49170e569d4be91a6
tomatino <tom.cnnng@eduge.ch>
2024-12-05 15:42:29 +0100

    trop beaux

--- js/script.js
+++ js/script.js
@@ -19,3 +19,37 @@ window.addEventListener("load", () => {
         }, index * 150);
     });
 });
+
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+    const submitButton = this.querySelector('button[type="submit"]');
+    const loadingMessage = document.createElement('p');
+    loadingMessage.innerHTML = 'Envoi en cours...';
+    this.appendChild(loadingMessage);
+    submitButton.disabled = true;
+
+    fetch(this.action, {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => {
+            loadingMessage.style.display = 'none';
+            submitButton.disabled = false;
+            if (response.ok) {
+                document.getElementById('messageVer').style.display = 'block';
+                document.getElementById('errorVer').style.display = 'none';
+            } else {
+                document.getElementById('errorVer').style.display = 'block';
+                document.getElementById('messageVer').style.display = 'none';
+            }
+        })
+        .catch(() => {
+            loadingMessage.style.display = 'none';
+            submitButton.disabled = false;
+            document.getElementById('errorVer').style.display = 'block';
+            document.getElementById('messageVer').style.display = 'none';
+        });
+});
